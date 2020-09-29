9528a9a1ad5c7816a1fb86176657636ef540d6a0
KNobHack <akudandia7272@gmail.com>
2020-09-29 12:22:48 +0700

    Migrasi tabel users

---
+++ app/Database/Migrations/2020-09-29-044232_buat_tabel_users.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class BuatTabelUsers extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id' => [
+				'type' => 'BIGINT',
+				'constraint' => 20,
+				'unsigned' => true,
+				'auto_increment' => true,
+			],
+			'email'  => [
+				'type' => 'VARCHAR',
+				'constraint' => '128',
+				'unique' => true,
+			],
+			'name' => [
+				'type' => 'VARCHAR',
+				'constraint' => '128',
+			],
+			'password' => [
+				'type' => 'VARCHAR',
+				'constraint' => '255',
+			],
+			'role' => [
+				'type' => 'ENUM',
+				'constraint' => ['Outsider', 'UPI Fam', 'Administrator'],
+			],
+			'created_at' => [
+				'type' => 'DATETIME',
+				'null' => true,
+			],
+			'updated_at' => [
+				'type' => 'DATETIME',
+				'null' => true,
+			],
+			'status' => [
+				'type' => 'ENUM',
+				'constraint' => ['Active', 'Blocked'],
+			],
+		]);
+		$this->forge->addKey('id', true);
+		$this->forge->createTable('users');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		$this->forge->dropTable('users');
+	}
+}
